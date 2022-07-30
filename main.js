bbe82cc22f5b187f32b54d6b91cc8dbc94879a8c
Alejandro Rodriguez <robertorodri110@gmail.com>
2022-07-30 20:41:43 -0300

    Hice la tabla del 3

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-alert("hola mundo!")
+alert("Vamos a probar una calculadora!");
+
+let numero1 = parseInt(prompt("Ingresa un numero, el mismo se multiplicara por 3"));
+
+for (numero1; numero1<=150; numero1++){
+    alert("Tu numero es:")
+    alert(numero1*3)
+    break
+}
+
+
+let dia = parseFloat(prompt("Ingresa el dia que pensas que juega boquita ♥"))
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
