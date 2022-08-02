7aa3fd05b1c80af312723761323fce6807d6705e
Alejandro Rodriguez <robertorodri110@gmail.com>
2022-08-02 19:42:37 -0300

    intento de hacer un menu semanal

--- main.js
+++ main.js
@@ -1,21 +1,62 @@
-alert("Vamos a probar una calculadora!");
+/*alert("Vamos a probar una calculadora!");
 
-let numero1 = parseInt(prompt("Ingresa un numero, el mismo se multiplicara por 3"));
+let numero1 = parseInt(prompt("Ingresa un numero"));
+let numero2 = parseInt(
+  prompt("Ingrese el numero por el que desea multiplicarlo")
+);
 
-for (numero1; numero1<=150; numero1++){
-    alert("Tu numero es:")
-    alert(numero1*3)
-    break
+for (numero1; numero1 <= 1000000000000000000000000000000; numero1++) {
+  alert("Tu numero es: " + numero1 * numero2);
+  console.log("Tu numero es: " + numero1 * numero2);
+  break;
 }
+alert("Muchas gracias, hasta pronto!");
+*/
+alert("Menu semanal!");
+console.log("Menu Semanal!");
+let dia = parseFloat(prompt("Ingrese el dia para ver el menu"));
+/*
+switch (dia) {
+  case "Lunes":
+    console.log("Lunes de Pizzas ");
+    break;
+  case "Martes":
+    console.log("Martes sin harinas");
+    break;
+  case "Miercoles":
+    console.log("Miercoles Veggie");
+    break;
+  case "Jueves":
+    console.log("Jueves de pastas: Ravioles o Sorrentinos");
+    break;
+  case "Viernes":
+    console.log("Viernes de empanadas fritas");
+    break;
+  case "Sabado":
+    console.log("Sabado de asado");
+    break;
+  case "Domingo":
+    console.log("Domingo libre eleccion");
+    break;
+  
+}*/
 
 
-let dia = parseFloat(prompt("Ingresa el dia que pensas que juega boquita ♥"))
+if (dia =="Lunes"){
+    console.log("Lunes de Pizzas");}
+    else if (dia == "Martes"){
+        console.log("Martes sin harinas");}
+    else if (dia == "Miercoles"){
+        console.log("Miercoles Veggie");}
+    else if (dia == "Jueves"){
+        console.log ("Jueves de pastas : Ravioles o sorrentinos");}
+    else if (dia == "Viernes"){
+        console.log ("Viernes de empanadas fritas");}
+    else if (dia == "Sabado"){
+        console.log ("Sabado de asado");}
+    else if (dia =="Domingo"){
+        console.log ("Domingo libre eleccion");}
+    else{
+        console.log("Elije un dia por favor!")
+                            }
 
-switch (key) {
-    case value:
-        
-        break;
-
-    default:
-        break;
-}
