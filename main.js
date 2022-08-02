c348af9521246e72ae4b0d72adcd7486a4b9a4b5
Alejandro Rodriguez <robertorodri110@gmail.com>
2022-08-02 20:57:11 -0300

    Terminado el algoritmo, hay 2. una calculadora de multiplicacion y un menu semanal selectivo de dias

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*alert("Vamos a probar una calculadora!");
+alert("Vamos a probar una calculadora!");
 
 let numero1 = parseInt(prompt("Ingresa un numero"));
 let numero2 = parseInt(
@@ -11,52 +11,44 @@ for (numero1; numero1 <= 1000000000000000000000000000000; numero1++) {
   break;
 }
 alert("Muchas gracias, hasta pronto!");
-*/
+
+/*----------------------------------------------------------*/
+
+
 alert("Menu semanal!");
 console.log("Menu Semanal!");
-let dia = parseFloat(prompt("Ingrese el dia para ver el menu"));
-/*
-switch (dia) {
-  case "Lunes":
-    console.log("Lunes de Pizzas ");
-    break;
-  case "Martes":
-    console.log("Martes sin harinas");
-    break;
-  case "Miercoles":
-    console.log("Miercoles Veggie");
-    break;
-  case "Jueves":
-    console.log("Jueves de pastas: Ravioles o Sorrentinos");
-    break;
-  case "Viernes":
-    console.log("Viernes de empanadas fritas");
-    break;
-  case "Sabado":
-    console.log("Sabado de asado");
-    break;
-  case "Domingo":
-    console.log("Domingo libre eleccion");
-    break;
-  
-}*/
+let dia = prompt("Ingrese el dia para ver el menu");
 
-
-if (dia =="Lunes"){
-    console.log("Lunes de Pizzas");}
-    else if (dia == "Martes"){
-        console.log("Martes sin harinas");}
-    else if (dia == "Miercoles"){
-        console.log("Miercoles Veggie");}
-    else if (dia == "Jueves"){
-        console.log ("Jueves de pastas : Ravioles o sorrentinos");}
-    else if (dia == "Viernes"){
-        console.log ("Viernes de empanadas fritas");}
-    else if (dia == "Sabado"){
-        console.log ("Sabado de asado");}
-    else if (dia =="Domingo"){
-        console.log ("Domingo libre eleccion");}
-    else{
-        console.log("Elije un dia por favor!")
-                            }
-
+while (dia != "salir") {
+  switch (dia) {
+    case "Lunes":
+      console.log("Lunes de Pizzas ");
+      alert("Lunes de Pizzas ");
+      break;
+    case "Martes":
+      console.log("Martes sin harinas");
+      alert("Martes sin harinas");
+      break;
+    case "Miercoles":
+      console.log("Miercoles Veggie");
+      alert("Miercoles Veggie");
+      break;
+    case "Jueves":
+      console.log("Jueves de pastas: Ravioles o Sorrentinos");
+      alert("Jueves de pastas: Ravioles o Sorrentinos");
+      break;
+    case "Viernes":
+      console.log("Viernes de empanadas fritas");
+      alert("Viernes de empanadas fritas");
+      break;
+    case "Sabado":
+      console.log("Sabado de asado");
+      alert("Sabado de asado");
+      break;
+    case "Domingo":
+      console.log("Domingo libre eleccion");
+      alert("Domingo libre eleccion");
+      break;
+  }
+  dia = prompt("Ingrese salir para terminar, de lo contrario seleccione dia");
+}
